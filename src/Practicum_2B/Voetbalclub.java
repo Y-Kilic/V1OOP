552f4905bf76e1e33e49c309f01e42ee1688ec4a
y-kilic <y-kilic@protonmail.com>
2020-02-06 14:36:33 +0100

    Practicum_2B Finished

--- src/Practicum_2B/Voetbalclub.java
+++ src/Practicum_2B/Voetbalclub.java
@@ -1,10 +1,14 @@
 package Practicum_2B;
 
 public class Voetbalclub {
-    int aantalGewonnen = 0;
-    int aantalVerloren = 0;
-    int aantalGelijk = 0;
-    String naam;
+    private int aantalGewonnen = 0;
+    private int aantalVerloren = 0;
+    private int aantalGelijk = 0;
+    private String naam;
+
+    public Voetbalclub(String naam) {
+        this.naam = naam;
+    }
 
     public void verwerkResultaat(char ch) {
         if (ch == 'w')
@@ -13,5 +17,19 @@ public class Voetbalclub {
             aantalGelijk = aantalGelijk + 1;
         if (ch == 'v')
             aantalVerloren = aantalVerloren + 1;
+    }
+    public int aantalPunten() {
+    var aantalPuntenWinst = this.aantalGewonnen * 3;
+    var aantalPuntenGelijk = this.aantalGelijk * 1;
+
+    return aantalPuntenWinst + aantalPuntenGelijk;
+    }
+
+    public int aantalGespeeld() {
+        return this.aantalGewonnen + this.aantalVerloren + this.aantalGelijk;
+    }
+
+    public String toString() {
+        return naam + " " + aantalGespeeld() + " " + aantalGewonnen + " " + aantalGelijk + " " + aantalVerloren + " " + aantalPunten();
     }
 }
